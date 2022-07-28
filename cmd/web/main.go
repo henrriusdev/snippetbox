a740b27efe9e6553cf39f5c4c825def1ec8aa302
hbourgeotjr <henrrybrgt@gmail.com>
2022-07-28 19:01:52 -0400

    Added InfoLog and Error Log

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/new", snippetCreate)
 
-	log.Println("Starting server...")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Println("Starting server...")
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
